497211bb272868f7d57dea1211c28524ede3d7ab
notmayae <mayae1251@gmail.com>
2026-03-23 22:28:55 +0200

    added "heartbeat" and timeout limits for the rabbit connections

--- main.py
+++ main.py
@@ -23,7 +23,12 @@ r = redis.Redis(
 
 # RabbitMQ Connection
 credentials = pika.PlainCredentials(os.getenv("RABBITMQ_USERNAME"), os.getenv("RABBITMQ_PASSWORD"))
-connection_params = pika.ConnectionParameters(os.getenv("RABBITMQ_HOST"), int(os.getenv("RABBITMQ_PORT")), '/', credentials)
+connection_params = pika.ConnectionParameters(os.getenv("RABBITMQ_HOST"), 
+                                              int(os.getenv("RABBITMQ_PORT")),
+                                              '/',
+                                              credentials,
+                                              heartbeat=3600,
+                                              blocked_connection_timeout=3600)
 connection = pika.BlockingConnection(connection_params)
 channel = connection.channel()
 
